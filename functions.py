033e8790e13d8e5edce84b469e3ba42f1608f6c9
francolz <francolazia@gmail.com>
2017-09-20 15:29:41 +0100

    adding functions file

---
+++ functions.py
@@ -0,0 +1,31 @@
+from math import sin, cos, sqrt, atan2, radians, pi # math stuff 
+from geopy.geocoders import Nominatim #get coordinates of locations 
+
+def printinfo (magnitude,time,day,location,depth):
+    return ('There was an earthquake of magnitude %s at %s (UTC) %s in %s.\
+    Depth was %s' % ( magnitude,time, day, location, depth))
+
+    #get lat and long of a place                                                                                                                                                         
+def get_location_lat_long (locate):
+    geolocator = Nominatim()
+    location = geolocator.geocode(locate)
+    return ((location.latitude, location.longitude))
+
+
+ #calculate distance between places taken form
+ #https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude                                                                                                                                                  
+def calculate_distance (lat_df,lat,lon_df, lon):
+    r_earth = 6373.0
+    lat1 = radians(lat_df)
+    lat2 = radians(lat)
+    long1 = radians(lon_df)
+    long2 = radians(lon)
+    dlong = long2 - long1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlong / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = r_earth * c
+    return distance
+
+def calculate_area (rad):
+  	return rad*rad*pi
